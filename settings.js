cac8a31158fd7be2eadd82e7970892371b547764
Eimo384 <eimoedueimo@gmail.com>
2025-07-01 22:24:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Unstoppable‎',
   botName: "Unstoppable Bot",
   botOwner: 'Unstoppable', // Your name
-  ownerNumber: '254741880641', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254111707007', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
