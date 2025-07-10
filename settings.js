ba024036be9dc7d528467225d79078c454157fc7
Eimo384 <eimoedueimo@gmail.com>
2025-07-10 15:16:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Simp4buko Bot',
-  author: 'Simp4buko‎',
-  botName: "Simp4buko Bot",
-  botOwner: 'Simp4buko', // Your name
-  ownerNumber: '254768662704', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Trevor Bot',
+  author: 'Trevor',
+  botName: "Trevor Bot",
+  botOwner: 'Trevor', // Your name
+  ownerNumber: '254745017772', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
